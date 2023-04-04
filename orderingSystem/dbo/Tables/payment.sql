5b3b3cca39fbcd423632e0a5710d4c8d55cf6396
cici0506 <cindykuang811@gmail.com>
2023-04-04 13:53:56 +1200

    added payment_type_id

--- orderingSystem/dbo/Tables/payment.sql
+++ orderingSystem/dbo/Tables/payment.sql
@@ -1,9 +1,10 @@
 ﻿CREATE TABLE [dbo].[payment] (
-    [payment_id]    INT IDENTITY(1, 1) NOT NULL,
-    [customer_id]   INT            NOT NULL,
-    [order_item_id] INT            NOT NULL,
-    [payment_date]  DATE           NOT NULL,
-    [total_amount]  DECIMAL (5, 2) NOT NULL,
+    [payment_id]      INT            IDENTITY (1, 1) NOT NULL,
+    [customer_id]     INT            NOT NULL,
+    [order_item_id]   INT            NOT NULL,
+    [payment_date]    DATE           NOT NULL,
+    [total_amount]    DECIMAL (5, 2) NOT NULL,
+    [payment_type_id] INT            NOT NULL,
     PRIMARY KEY CLUSTERED ([payment_id] ASC),
     FOREIGN KEY ([customer_id]) REFERENCES [dbo].[customer_details] ([customer_id]) ON DELETE CASCADE ON UPDATE CASCADE
 );
